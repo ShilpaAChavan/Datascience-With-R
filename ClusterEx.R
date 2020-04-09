bafffc83051b27b998fbdcb3fd9420d29a03df6d
ShilpaAChavan <61113242+ShilpaAChavan@users.noreply.github.com>
2020-04-09 21:23:57 +0530

    Hierarchical cluster Algorithm

--- ClusterEx.R
+++ ClusterEx.R
@@ -1,10 +1,20 @@
+
+##################### Hierarchial Clustering #################################
 mydata1 <- read.csv("D:/Shilpa/Datascience/classroom/dataset/Universities.csv")
 mydata <- scale(mydata1[,2:7])
 
-d <-dist(mydata,method = "euclidean")#computing the distance matrix
-fit <- hclust(d,method="centroid") #average Building the algorithm
-plot(fit)#display dendogram
-groups<- cutree(fit,k=4) #cut tree into 4 cluster
+#computing the distance matrix
+d <-dist(mydata,method = "euclidean")
+
+#Building the algorithm.
+#distance between clusters calculated using Centroid Method(Average method)
+fit <- hclust(d,method="centroid") 
+
+#display dendogram
+plot(fit)
+
+#cut tree into 4 cluster
+groups<- cutree(fit,k=4) 
 
 rect.hclust(fit,k=4,border="red")
 clusters = data.frame('Uni'=mydata1[,1],'Cluster'=groups)
